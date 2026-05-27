8e0e2ac1d68508658d5a05ae83cd2b5c7110fa05
Thành Nguyễn <tn113805@gmail.com>
2026-05-27 15:08:01 +0700

    feat: add farewell function

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function greet(name) {
   return `Hello, ${name}!`;
 }
-module.exports = { greet };
+function farewell(name) {
+  return `Goodbye, ${name}!`;
+}
+module.exports = { greet, farewell };
